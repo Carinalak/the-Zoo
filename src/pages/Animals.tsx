9fd087a8f7add280c70ed357e50ad4e24b27909b
Carinalak <carina.lakosil@gmail.com>
2024-08-31 14:53:59 +0200

    :lipstick: Layout-fixing

--- src/pages/Animals.tsx
+++ src/pages/Animals.tsx
@@ -19,6 +19,29 @@ const getBackgroundColorClass = (lastFed: string | null) => {
     }
 };
 
+const getHungryWarning = (animalName: string, lastFed: string | null) => {
+    if (!lastFed) {
+        return `${animalName} är jättehungrig!`;
+    }
+    
+    const lastFedTime = new Date(lastFed).getTime();
+    const currentTime = new Date().getTime();
+    const hoursSinceFed = (currentTime - lastFedTime) / (1000 * 60 * 60);
+
+    if (hoursSinceFed < 3) {
+        return `${animalName} är mätt!`;
+    }
+
+      if (hoursSinceFed >= 3 && hoursSinceFed < 4) {
+        return `${animalName} är hungrig!`;
+    }
+    
+    if (hoursSinceFed >= 4) {
+        return `${animalName} är jättehungrig!`;
+    }
+    return null;
+};
+
 export const Animals = () => {
     const animals = useLoaderData() as IAnimals[];
 
@@ -44,6 +67,12 @@ export const Animals = () => {
                                     onError={handleImageError}
                                 />
                             </Link>
+                          
+                            {getHungryWarning(animal.name, animal.lastFed) && (
+                                <span className="hungry-warning">
+                                    {getHungryWarning(animal.name, animal.lastFed)}
+                                </span>
+                            )}
                         </div>
                     ))}
                 </article>
