87e0170ed7000be7fdb9065798c8e674bc04bb83
Carinalak <carina.lakosil@gmail.com>
2024-09-01 13:53:45 +0200

    :lipstick: Layout

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -3,17 +3,20 @@ import { NavLink, Outlet } from "react-router-dom"
 export const Layout = () => {
     return <>
         <header>
+            <section className="navigation-container">
             <h1>The Zoo</h1>
-            <nav>
-            <ul>
-                <li>
-                    <NavLink to={"/"}>Hem</NavLink>
-                </li>
-                <li>
-                    <NavLink to={"/animals"}>Djuren</NavLink>
-                </li>
-            </ul>
-        </nav>
+                <nav>
+                
+                <ul>
+                    <li>
+                        <NavLink to={"/"}>Hem</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to={"/animals"}>Djuren</NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </section>
         </header>
         <main>
             <Outlet></Outlet>
