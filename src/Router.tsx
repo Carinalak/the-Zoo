0684229a7939ca189dcbede0e9d938d8684c52e9
Carinalak <carina.lakosil@gmail.com>
2024-09-02 13:21:24 +0200

    :rocket: Changing back to browserRouter

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,4 +1,4 @@
-import { createHashRouter } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import { Layout } from "./pages/Layout";
 import { animalLoader, animalsLoader } from "./loaders/animalLoader";
 import { Animals } from "./pages/Animals";
@@ -11,7 +11,7 @@ import { feedAnimalLoader } from "./loaders/feedAnimalLoader";
 import { hungryAnimalsLoader } from "./loaders/hungryAnimalsLoader";
 
 
-export const router = createHashRouter([
+export const router = createBrowserRouter([
     {
         path: "/",
         element: <Layout></Layout>,
--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import * as React from 'react'
-import { HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HashRouter>
     <App />
-    </HashRouter>
   </React.StrictMode>,
 )
